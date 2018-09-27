332269324836ff79512b45ffa49097aba9482ffc
eder_matheus <emrmonteiro@inf.ufrgs.br>
2018-09-27 14:03:45 -0300

    Funcoes de gerenciamento do escalonador (adicionar e remover das filas) implementadas. Precisa depurar

--- cthread/include/escalonador.h
+++ cthread/include/escalonador.h
@@ -1,4 +1,5 @@
 #include "../include/support.h"
+#include "../include/cdata.h"
 
 PFILA2 __aptos_prio_0;
 PFILA2 __aptos_prio_1;
@@ -7,3 +8,9 @@ PFILA2 __executando;
 PFILA2 __bloqueados;
 
 int inicializaFilas();
+
+int insereEmApto(TCB_t *thread);
+
+int insereEmExecutando(TCB_t *thread);
+
+int insereEmBloqueado(TCB_t *thread);
--- cthread/src/escalonador.c
+++ cthread/src/escalonador.c
@@ -11,3 +11,119 @@ int inicializaFilas() {
 
 	return retorno;
 }
+
+// ----------------------------------------------------------------------------------- //
+
+int insereEmApto(TCB_t *thread) {
+	int prioridade = thread->prio;
+	int success;
+
+	if (prioridade == 0) {
+		success = AppendFila2(__aptos_prio_0, thread);
+	} else if (prioridade == 1) {
+		success = AppendFila2(__aptos_prio_1, thread);
+	} else if (prioridade == 2) {
+		success = AppendFila2(__aptos_prio_2, thread);
+	} else{ 
+		return -1;
+	}
+
+	return 0;
+}
+
+// ----------------------------------------------------------------------------------- //
+
+int insereEmExecutando(TCB_t *thread) {
+	int success;
+
+	success = AppendFila2(__executando, thread);
+	return success;
+}
+
+// ----------------------------------------------------------------------------------- //
+
+int insereEmBloqueado(TCB_t *thread) {
+	int success;
+
+	success = AppendFila2(__bloqueados, thread);
+	return success;
+}
+
+// ----------------------------------------------------------------------------------- //
+
+int retiraDeExecutando() {
+	int estado_executando;
+	int estado_iterador;
+
+	estado_iterador = FirstFila2(__executando);
+	if (estado_iterador == 0) {
+		estado_executando = DeleteAtIteratorFila2(__executando);
+	}
+
+	if (estado_executando != DELITER_VAZIA) {
+		printf ("Erro ao retirar de executando: fila nao esta vazia\n");
+	}
+
+	return 0;
+}
+
+// ----------------------------------------------------------------------------------- //
+
+int retiraDeApto() {
+	int estado_apto;
+	int estado_iterador;
+
+	estado_iterador = FirstFila2(__aptos_prio_0);
+	if (estado_iterador == 0) { // fila nao vazia
+		estado_apto = DeleteAtIteratorFila2(__aptos_prio_0);
+		if (estado_apto == 0 || estado_apto == DELITER_VAZIA) {
+			return 0;
+		}
+		else {
+			return -1;
+		}
+	}
+
+	estado_iterador = FirstFila2(__aptos_prio_1);
+	if (estado_iterador == 0) { // fila nao vazia
+		estado_apto = DeleteAtIteratorFila2(__aptos_prio_1);
+		if (estado_apto == 0 || estado_apto == DELITER_VAZIA) {
+			return 0;
+		}
+		else {
+			return -1;
+		}
+	}
+
+	estado_iterador = FirstFila2(__aptos_prio_2);
+	if (estado_iterador == 0) { // fila nao vazia
+		estado_apto = DeleteAtIteratorFila2(__aptos_prio_2);
+		if (estado_apto == 0 || estado_apto == DELITER_VAZIA) {
+			return 0;
+		}
+		else {
+			return -1;
+		}
+	}
+
+	return -1;
+}
+
+// ----------------------------------------------------------------------------------- //
+
+int retiraDeBloqueado() {
+	int estado_bloqueado;
+	int estado_iterador;
+
+	estado_iterador = FirstFila2(__bloqueados);
+	if (estado_iterador == 0) {
+		estado_bloqueado = DeleteAtIteratorFila2(__bloqueados);
+	}
+
+	if (estado_bloqueado == 0 || estado_bloqueado == DELITER_VAZIA) {
+		return 0;
+	}
+
+	return -1;
+}
+
